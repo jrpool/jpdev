3d99e960c9cad4407bf7664692a0b3cb82f7261e
Jonathan Pool <pool@stulta.com>
2021-07-21 21:47:15 -0400

    Continued developing maxgood

--- blog/entries/0000/maxgood.js
+++ blog/entries/0000/maxgood.js
@@ -1,45 +1,123 @@
-// Moves the focal outline within a menu.
-const setOutline = menu => {
-  const menuItems = Array.from(menu.getElementsByTagName('a'));
-  menuItems.forEach(menuItem => {
-    menuItem.class = menuItem.id === menu.ariaActivedescendant ? 'focal' : 'blurred';
+// Opens a menu.
+const openMenu = button => {
+  button.ariaExpanded === 'true';
+  const menu = document.getElementById(button.ariaControls);
+  menu.className = 'open';
+};
+// Closes a menu.
+const closeMenu = button => {
+  button.ariaExpanded === 'false';
+  const menu = document.getElementById(button.ariaControls);
+  menu.className = 'shut';
+};
+// Makes the specified menu item (the last if itemIndex is -1) active.
+const setActive = (menu, itemIndex) => {
+  const menuItems = Array.from(menu.querySelectorAll('[role=menuitem]'));
+  menuItems.forEach((item, index) => {
+    if (itemIndex === -1 && index === menuItems.length - 1 || index === itemIndex) {
+      item.className = 'focal';
+      menu.ariaActivedescendant = item.id;
+    }
+    else {
+      item.className = 'blurred';
+    }
   });
 };
-// Toggles a menu open or closed.
-const toggleMenu = button => {
-  const isOpen = button.ariaExpanded === "true";
-  const menu = document.getElementById(button.ariaControls);
-  if (isOpen) {
-    menu.className = 'shut';
-    button.ariaExpanded = 'false';
-  }
-  else {
-    menu.className = 'open';
-    button.ariaExpanded = 'true';
-    menu.focus();
-    setOutline(menu);
-  }
-  menu.className = isOpen ? 'shut' : 'open';
-  button.ariaExpanded = isOpen ? 'false' : 'true';
-  if (! isOpen) {
-    menu.focus();
-  }
-};
-// Sets the active menu item.
-const setActive = (menu, whither) => {
-  const menuItemIDs = Array.from(menu.getElementsByTagName('a')).map(menuItem => menuItem.id);
+// Returns the index of a keyboard-chosen menu item.
+const chooseMenuItem = (menu, key) => {
+  const menuItems = Array.from(menu.querySelectorAll('[role=menuitem]'));
+  const menuItemIDs = menuItems.map(item => item.id);
   const activeIndex = menuItemIDs.indexOf(menu.ariaActivedescendant);
+  const menuItemCount = menuItems.length;
   let newIndex;
-  if (whither === 'down') {
-    newIndex = activeIndex + 1;
+  if (key === 'ArrowDown') {
+    newIndex = (activeIndex + 1) % menuItemCount;
+  }
+  else if (key === 'ArrowUp') {
+    newIndex = (menuItemCount + activeIndex - 1) % menuItemCount;
+  }
+  else if (key === 'Home') {
+    newIndex = 0;
+  }
+  else if (key === 'End') {
+    newIndex = menuItemCount - 1;
+  }
+  else if (/^[a-zA-Z]+$/.test(key)) {
+    newIndex = menuItems
+    .map(
+      (item, index) => item.textContent.toLowerCase().startsWith(spec.toLowerCase()) ? index : -1
+    )
+    .filter(index => index > -1 && index > activeIndex)[0];
   }
 };
-  // Add the manager username and computed email to the query.
-  globals.queryExpandMan(query, 'man');
-  if (globals.queryIncludes(query, ['man', 'manUserName'])) {
-    globals.render(query, 'addUser2', true, response);
+// Activates the active menu item.
+const activate = menu => {
+  const activeItem = document.getElementById(menu.activeDescendant);
+  activeItem.dispatchEvent(new Event('click'));
+};
+// Handles click activation of a menu button.
+const menuButtonClickHandler = button => {
+  if (button.ariaExpanded === 'true') {
+    closeMenu(button);
   }
   else {
-    globals.serveMessage('ERROR: Your code missing or invalid.', response);
+    openMenu(button);
+    const menu = document.getElementById(button.ariaControls);
+    if (! menu.activeDescendant) {
+      setActive(menu, 0);
+    }
   }
 };
+// Handles keyboard activation of a menu button.
+const menuButtonKeyHandler = (button, key) => {
+  openMenu(button);
+  const menu = document.getElementById(button.ariaControls);
+  if (key === 'ArrowUp') {
+    setActive(menu, -1);
+  }
+  else {
+    setActive(menu, 0);
+  }
+};
+// Handles keyboard operations in a menu.
+const menuKeyHandler = menu => {
+  const menuItems = Array.from(menu.querySelectorAll('[role=menuitem]'));
+
+};
+// OPERATIONS
+const defButton = document.getElementById('defButton');
+defButton.addEventListener('click', menuButtonClickHandler);
+document.body.addEventListener('keyup', event => {
+  const key = event.key;
+  if (
+    document.activeElement === defButton
+    && ['Enter', 'Space', 'ArrowDown', 'ArrowUp'].includes(key)
+  ) {
+    menuButtonKeyHandler(defButton, key);
+  }
+});
+const defMenu = document.getElementById('defMenu');
+document.body.addEventListener('keyup', event => {
+  const key = event.key;
+  if (
+    document.activeElement === defMenu
+    && (['ArrowDown', 'ArrowUp', 'Home', 'End'].includes(key) || /^[a-zA-Z]+$/.test(key))
+  ) {
+    const newIndex = chooseMenuItem(defMenu, key);
+  }
+});
+defMenu.addEventListener('click', event => {
+  event.preventDefault();
+  const target = event.target;
+  if (target.role === 'menuitem') {
+    closeMenu(defButton);
+    target.dispatchEvent(new Event('click'));
+  }
+});
+document.body.addEventListener('keyup', event => {
+  if (document.activeElement === defMenu && event.key === 'Enter') {
+    const activeItem = document.getElementById(defMenu.activeDescendant);
+    closeMenu(defButton);
+    activeItem.dispatchEvent(new Event('click'));
+  }
+});
